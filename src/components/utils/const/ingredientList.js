e266563c05b43d299eca9a241c9eab2cf4d7dd79
Agucarpar <agucp@outlook.es>
2020-10-18 11:39:33 +0200

    change tomatoe price

--- src/components/utils/const/ingredientList.js
+++ src/components/utils/const/ingredientList.js
@@ -1,11 +1,10 @@
 
-
 const ingredientsList= [
-    {ingredient:"tomatoe", price:0.6,},
-    {ingredient:"salad", price:0.5,},
-    {ingredient:"bacon",price: 1.25,},
-    {ingredient:"meat", price: 3.5,},
-    {ingredient:"cheese", price: 0.75,},
+    {ingredient:"tomatoe", price:0.70},
+    {ingredient:"salad", price:0.5},
+    {ingredient:"bacon",price: 1.25},
+    {ingredient:"meat", price: 3.5},
+    {ingredient:"cheese", price: 0.75},
 ]
 
 
